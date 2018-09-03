3a917843faac102f0fdf3508df7b95cd1e6b599d
akostenko <alinakostenko91@gmail.com>
2018-09-04 01:09:54 +0300

    exam2.0

--- exam/js/script.js
+++ exam/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    //Slider Our Team
     $('#team_slider').slick({
         infinite: true,
         dots: true,
@@ -8,6 +9,7 @@ $(document).ready(function(){
         arrows: false
     });
 
+    //Slider Testimonials
     $('#test_slider').slick({
         infinite: true,
         dots: true,
@@ -16,6 +18,36 @@ $(document).ready(function(){
         arrows: false
     });
 
+    //Scrolling
+    var $anchors = $('a[href^="#"]').not('[href="#"]');
+
+    $anchors.click(function (e) {
+        e.preventDefault();
+
+        var id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top
+        }, 1000);
+    });
+
+    //Isotope
+    var $isotopeGrid = $('.portfolio-images');
+
+    $isotopeGrid.isotope({
+        itemSelector: '.img-wrapper',
+        percentPosition: true
+    });
+
+    $('.isotop .filters button').click(function () {
+        var filterValue = $(this).data('filter');
+
+        $isotopeGrid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //Google Map
     showMap(document.getElementById("map"), 49.568629, 34.585370);
 
     function showMap(mapElement, lat, lon) {
@@ -23,15 +55,17 @@ $(document).ready(function(){
 
         var marker = new google.maps.Marker({
             position: center,
+            icon:'../img/marker.png'
         });
 
         var mapProp= {
             center: center,
-            zoom: 10,
+            zoom: 16,
             disableDefaultUI: true,
             mapTypeId: 'terrain'
         };
-        var map =new google.maps.Map(mapElement, mapProp);
+
+        var map = new google.maps.Map(mapElement, mapProp);
         marker.setMap(map);
     }
 });
